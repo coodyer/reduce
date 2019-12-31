7018047317c9ed5326e25826f3e875d6224b92fa
Coody <644556636@qq.com>
2019-12-31 22:16:04 +0800

    我也不知道为嘛要提交

--- service/src/test/java/reduce/Test.java
+++ service/src/test/java/reduce/Test.java
@@ -4,11 +4,10 @@ import java.math.BigDecimal;
 
 public class Test {
 
-	private final String format = "yyyy-MM-HH";
-
 	public static void main(String[] args)
 			throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		System.out.println(new BigDecimal("738642.144600000000000000").add(new BigDecimal("93708.189800000000000000").add(new BigDecimal("38256.347500000000000000"))));
+		System.out.println(new BigDecimal("738642.144600000000000000")
+				.add(new BigDecimal("93708.189800000000000000").add(new BigDecimal("38256.347500000000000000"))));
 	}
 
 }
