44e13820701e0b1755bd0aecc9c1bd6a5982e260
Coody <644556636@qq.com>
2020-01-05 03:55:09 +0800

    新增登录注册

--- service/src/test/java/reduce/Test.java
+++ service/src/test/java/reduce/Test.java
@@ -1,9 +1,17 @@
 package reduce;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.coody.framework.core.util.DateUtils;
+
 public class Test {
 
-	public static void main(String[] args)
-			throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException {
+	public static void main(String[] args) {
+
+		Date time = DateUtils.toDate("2019-01-04 12:32:11");
+		time = DateUtils.change(time, Calendar.DAY_OF_YEAR, 200);
+		System.out.println(DateUtils.toString(time,"yyyy-MM-dd"));
 	}
 
 }
