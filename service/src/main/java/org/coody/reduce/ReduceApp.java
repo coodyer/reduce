5115ef276cced2c6ebb8105c91be51e44f8291dc
Coody <644556636@qq.com>
2020-01-07 18:22:45 +0800

    升级版本

--- service/src/main/java/org/coody/reduce/ReduceApp.java
+++ service/src/main/java/org/coody/reduce/ReduceApp.java
@@ -1,12 +1,8 @@
 package org.coody.reduce;
 
-import org.coody.framework.minicat.CoreApp;
-import org.coody.reduce.common.servlet.ReduceServlet;
-
 public class ReduceApp {
 
 	public static void main(String[] args) throws Exception {
-
-		CoreApp.init(ReduceServlet.class);
+		// CoreApp.init(ReduceServlet.class);
 	}
 }
