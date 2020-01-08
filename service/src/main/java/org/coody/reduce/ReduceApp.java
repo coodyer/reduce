9aa3e776194f2eab704d10aba259d3357dced938
Coody <644556636@qq.com>
2020-01-08 17:19:46 +0800

    修改类名

--- service/src/main/java/org/coody/reduce/ReduceApp.java
+++ service/src/main/java/org/coody/reduce/ReduceApp.java
@@ -1,11 +1,11 @@
 package org.coody.reduce;
 
-import org.coody.framework.minicat.CoreApp;
+import org.coody.framework.minicat.MinicatApp;
 import org.coody.reduce.common.servlet.ReduceServlet;
 
 public class ReduceApp {
 
 	public static void main(String[] args) throws Exception {
-		CoreApp.init(ReduceServlet.class);
+		MinicatApp.init(ReduceServlet.class);
 	}
 }
